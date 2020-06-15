21f84d71ff11d5a0b1853dfea111251d8214223a
RenatoValentim <rvrenato28@gmail.com>
2020-06-14 22:51:55 -0300

    Altere a variavael final_salary para camel case

--- problems/beginner/1009.js
+++ problems/beginner/1009.js
@@ -3,12 +3,12 @@ const FIXED_SALRY = 1700.00;
 const SALES_TOTAL_MONTH_IN_MONEY = 1230.50;
 
 function salaryBonusCalculation(salesTotalMonthInMoney, fixedSalry, percentage) {
-  let final_salary;
+  let finalSalary;
   if (percentage !== undefined | null) {
-    final_salary = ((salesTotalMonthInMoney * percentage) / 100) + fixedSalry;
+    finalSalary = ((salesTotalMonthInMoney * percentage) / 100) + fixedSalry;
   }
 
-  return final_salary;
+  return finalSalary;
 }
 
 const FINAL_SALARY = salaryBonusCalculation(SALES_TOTAL_MONTH_IN_MONEY, FIXED_SALRY, 15);
