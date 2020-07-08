4d97ce919f70cead1879bbc3f7004f81c2a737d2
RenatoValentim <rvrenato28@gmail.com>
2020-07-08 11:50:20 -0300

    Resolva 1018

--- beginner/1018.ts
+++ beginner/1018.ts
@@ -4,18 +4,11 @@ import * as path from "path";
 const myPath = path.join(__dirname, "file.txt");
 const input = fs.readFileSync(myPath, "utf8");
 
+let valueInput = Number(input);
 const CEDULAS = [100, 50, 20, 10, 5, 2, 1];
 
-const obterMenorNumeroCedulasPossivel = (cedulas: number[], dividendoPelasCedulas: number) => {
-  const cedulasValidas = []
-  cedulas.map(cedula => {
-    if (dividendoPelasCedulas % cedula === 0) {
-      cedulasValidas.push(cedula);
-    }
-  })
-  console.log(cedulasValidas);
-  return cedulasValidas;
-}
-
-console.log(input);
-console.log(obterMenorNumeroCedulasPossivel(CEDULAS, Number(input)));
+console.log(valueInput);
+CEDULAS.map(cedula => {
+  console.log(Math.trunc(valueInput / cedula) + " nota(s) de R$ " + cedula.toFixed(2).replace('.', ','));
+  valueInput = valueInput % cedula;
+});
