7ee76fb7f6c95f8359188bdced87c1025e67463d
Renato Valentim <48966099+RenatoValentim@users.noreply.github.com>
2022-01-05 21:56:04 -0300

    solve 1006

--- beginner/1006.c
+++ beginner/1006.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main() {
-
+    float a, b, c;
+    float const PA = 2;
+    float const PB = 3;
+    float const PC = 5;
+    scanf("%f%f%f", &a, &b, &c);
+    if (a > 10 || b > 10 || c > 10) {
+      printf("As notas devem ser abaixo de 10.0\n");
+      return 0;
+    }
+    float average = ((a * PA) + (b * PB) + (c * PC)) / (PA + PB + PC);
+    printf("MEDIA = %.1f\n", average);
+    return 0;
 }
